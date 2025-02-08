9452fcadd21c8c41e32ae4b697dcdce377ac3a11
Alzubair98 <203721133@ogr.altinbas.edu.tr>
2025-02-09 02:23:49 +0300

    add the nav

--- app.vue
+++ app.vue
@@ -1,6 +1,16 @@
 <template>
   <div>
     <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+    <!-- NavBar -->
+    <header
+      class="sticky top-0 z-50 flex justify-between items-center space-x-1 border-b bg-white p-4 shadow-md"
+    >
+      <a href="" class="text-3xl font-mono">cartreader</a>
+    </header>
+    <!-- NavBar -->
+
+    <!-- Home Hero-->
+
+    <!-- Home Hero-->
   </div>
 </template>
