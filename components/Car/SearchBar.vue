e8ddbfd622b4d975ed779d421e24abcb9dc69ddd
Alzubair98 <203721133@ogr.altinbas.edu.tr>
2025-06-10 22:29:47 +0300

    refactor SearchBar and navBar components; implement search functionality and update link to use NuxtLink

--- components/Car/SearchBar.vue
+++ components/Car/SearchBar.vue
@@ -6,7 +6,24 @@
       type="text"
       class="py-3 px-5 w-full text-2xl rounded-full focus:outline-none"
       placeholder="Search by City ..."
+      v-model="city"
     />
-    <button class="bg-sky-500 px-10 text-white">Search</button>
+    <button class="bg-sky-500 px-10 text-white" @click="handleSearch">
+      Search
+    </button>
   </div>
 </template>
+
+<script setup lang="ts">
+const city = ref<string>("");
+const router = useRouter();
+
+const handleSearch = () => {
+  if (city.value.trim() === "") {
+    alert("Please enter a city name.");
+    return;
+  }
+  // Navigate to the city page with the entered city name
+  router.push(`/city/${city.value}/car`);
+};
+</script>
--- components/navBar.vue
+++ components/navBar.vue
@@ -2,6 +2,6 @@
   <header
     class="sticky top-0 z-50 bg-violet-200 flex justify-between items-center space-x-1 p-4 shadow-md"
   >
-    <a href="" class="text-3xl font-mono">Car Trader</a>
+    <NuxtLink to="/" class="text-3xl font-mono">Car Trader</NuxtLink>
   </header>
 </template>
