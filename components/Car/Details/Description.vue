97dd8c9ed7ce8097d9ba39401c92d7663bee920c
Alzubair98 <203721133@ogr.altinbas.edu.tr>
2025-06-10 22:23:04 +0300

    add car details components: Attributes, Contact, Description, Hero; update car details page layout

---
+++ components/Car/Details/Description.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="mt-5">
+    <p class="mb-10">
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad quia ipsum
+      suscipit nostrum nulla voluptatum velit consectetur, tenetur corrupti
+      quasi reiciendis soluta vel enim error odit doloremque, rerum molestias
+      quo.
+    </p>
+  </div>
+</template>
---
+++ components/Car/Details/Hero.vue
@@ -0,0 +1,25 @@
+<template>
+  <!-- Car Hero-->
+  <div>
+    <div class="mt-10">
+      <img
+        src="https://cdn.bmwblog.com/wp-content/uploads/2015/05/2015-bmw-3-series-sedan-images-07.jpg"
+        class="w-full"
+        alt="Car Image"
+      />
+      <h1 class="mt-10 text-xl">BMW M-5</h1>
+      <div
+        class="text-slate-500 flex text-lg mt-3 border-b pb-5 justify-between"
+      >
+        <div class="flex">
+          <p class="mr-2">5 Seats</p>
+          <p class="mr-2">| 1400 miles</p>
+        </div>
+        <div>
+          <p class="font-bold text-2xl">$25,555</p>
+        </div>
+      </div>
+    </div>
+  </div>
+  <!-- Car Hero-->
+</template>
