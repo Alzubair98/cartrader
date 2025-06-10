19d62b02f511d670cec32a41ace75e95317aab8e
Alzubair98 <203721133@ogr.altinbas.edu.tr>
2025-06-10 15:36:35 +0300

    add CarCard, cards, and sideBar components; update city and make page layout

---
+++ components/Car/card.vue
@@ -0,0 +1,23 @@
+<template>
+  <div
+    class="shadow border w-full overflow-hidden mb-5 cursor-pointer h-[200px]"
+  >
+    <div class="flex h-full">
+      <img
+        src="https://cdn.bmwblog.com/wp-content/uploads/2015/05/2015-bmw-3-series-sedan-images-07.jpg"
+        alt="none"
+        class="w-[300px] h-full"
+      />
+      <div class="p-4 flex flex-col">
+        <div>
+          <h1 class="text-2xl text-blue-700">BMW M-5</h1>
+          <p class="text-gray-700">
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Adipisci
+            dolor
+          </p>
+          <h1 class="mt-auto text-xl">$39,555</h1>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
