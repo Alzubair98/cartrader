7661c7f3aac4a22bcc515811e3dc6fcc68208f3c
Alzubair98 <203721133@ogr.altinbas.edu.tr>
2025-06-14 17:28:41 +0300

    refactor car details components to use dynamic data for features and description

--- components/Car/Details/Description.vue
+++ components/Car/Details/Description.vue
@@ -1,10 +1,13 @@
 <template>
   <div class="mt-5">
     <p class="mb-10">
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad quia ipsum
-      suscipit nostrum nulla voluptatum velit consectetur, tenetur corrupti
-      quasi reiciendis soluta vel enim error odit doloremque, rerum molestias
-      quo.
+      {{ desc }}
     </p>
   </div>
 </template>
+
+<script setup lang="ts">
+const props = defineProps<{
+  desc: string;
+}>();
+</script>
