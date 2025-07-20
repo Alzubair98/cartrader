a9c318029c7d0c95fb9310fdc5df0141ada2c37d
Alzubair98 <zubairsk53@gmail.com>
2025-07-20 13:52:14 +0300

    add message card

---
+++ components/Car/MessageCard.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="even:bg-gray-100 p-4">
+    <h1 class="font-bold text-2xl">Laith Harb</h1>
+    <p class="text-gray-700">
+      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Consectetur,
+      nostrum natus velit molestiae neque sint quas quod sapiente voluptatibus
+      autem.
+    </p>
+
+    <div class="flex items-center mt-5">
+      <p class="text-green-700 mr-10">alzu@gmail.com</p>
+      <p class="text-blue-500">555-555-5555</p>
+    </div>
+  </div>
+</template>
