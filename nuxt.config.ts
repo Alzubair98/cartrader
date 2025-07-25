1e7a2ca9d88134fe5a28cf1c1d635ee9c3cd5c83
alzubair98 <zubairsk53@gmail.com>
2025-07-25 19:26:43 +0300

    solve the redirect option

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,10 @@ export default defineNuxtConfig({
   compatibilityDate: "2025-05-15",
   devtools: { enabled: true },
 
+  supabase: {
+    redirect: false,
+  },
+
   modules: [
     "@nuxtjs/tailwindcss",
     "@nuxt/content",
