f1895ab91c19d31df1f3402cf22edbce9c48d7f6
alzubair98 <zubairsk53@gmail.com>
2025-07-26 16:52:57 +0300

    add the query from the url

--- server/api/cars/[city].ts
+++ server/api/cars/[city].ts
@@ -16,5 +16,17 @@ export default defineEventHandler((event) => {
     });
   }
 
+  if (minPrice) {
+    filteredCars = filteredCars.filter((car) => {
+      return car.price >= parseInt(minPrice);
+    });
+  }
+
+  if (maxPrice) {
+    filteredCars = filteredCars.filter((car) => {
+      return car.price <= parseInt(maxPrice);
+    });
+  }
+
   return filteredCars;
 });
